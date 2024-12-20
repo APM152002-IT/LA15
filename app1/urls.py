32b8ee1b0ec1a0ecdb8a0d4b57636523281e9679
Adrian Malabanan <adrianmalabanan152002@gmail.com>
2024-12-20 20:29:02 +0800

    initial comit

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Root/home view
+    path('hello/', views.say_hello, name='say_hello'),
+    path('hi/', views.say_hi, name='say_hi'),
+    path('posts/', views.blog_list, name='blog_list'),
+    path('posts/<int:id>/', views.blog_detail, name='blog_detail'),  # Dynamic ID for blog details
+]
